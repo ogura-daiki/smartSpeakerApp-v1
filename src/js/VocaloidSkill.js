23e0dac702539ffa4911a4a86b7bbdc629c1e0e7
OGURA DAIKI <dkog2001@gmail.com>
2023-02-27 21:20:38 +0900

    再生リストの再生を仮実装

--- src/js/VocaloidSkill.js
+++ src/js/VocaloidSkill.js
@@ -2,6 +2,12 @@ import Reply from "./Reply.js";
 import { Command, Skill, Slot } from "./Skill.js";
 import Romanizer from "https://cdn.skypack.dev/js-hira-kata-romanize";
 
+const tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 const r = new Romanizer({
   chouon: Romanizer.CHOUON_ALPHABET,
 });
@@ -9,6 +15,47 @@ const r = new Romanizer({
 const VocaloidSkill = Skill("vocaloidBot", Slot(/ボカ\s*ロ\s*([ボポ]ット|bot|BOT|Bot)[\s\S]*/));
 
 const searchVocaloidP = name => fetch(`https://script.google.com/macros/s/AKfycbykdy4aTpYdT3pu4GLS7R4BKNbuCFyTH4xWlxrFDoPuBSw1A2sAFN7KdezjRfGFmjsj/exec?q=${name}`).then(r=>r.json())
+
+const playerTask = {
+  p:[],
+  listener(){
+    this.p.forEach((r)=>r());
+    this.p = [];
+  },
+  wait(callback){
+    let resolve;
+    const promise = new Promise(r=>{resolve = r});
+    this.p.push(resolve);
+    callback();
+    return promise;
+  },
+};
+
+const createPlayer = async (id, playlistId)=>{
+  return new Promise(r=>{
+    const player = new YT.Player(id, {
+      height: '360',
+      width: '640',
+      playerVars:{
+        list: playlistId,
+        listType: 'playlist',
+      },
+      events: {
+        'onReady':function(event){r(event.target)},
+        "onStateChange": e=>{
+          try{
+            console.log({e});
+            playerTask.listener();
+          }
+          catch{};
+        },
+        'onError':e=>{
+          console.log(e);
+        }
+      }
+    });
+  }) 
+}
 
 VocaloidSkill.defineCommands({
   playProducer:Command({
@@ -28,8 +75,10 @@ VocaloidSkill.defineCommands({
         ];
       }
 
+      const player = await createPlayer("temp", list[0][1]);
+      player.playVideo();
       return [
-        Reply.Link(list.map(item=>({url:`https://www.youtube.com/watch?v=&list=${item[1]}`, title:item[0], description:`${item[0]} のYoutubeチャンネル`})))
+        //Reply.Link(list.map(item=>({url:`https://www.youtube.com/watch?v=&list=${item[1]}`, title:item[0], description:`${item[0]} のYoutubeチャンネル`})))
       ];
     }
   })
